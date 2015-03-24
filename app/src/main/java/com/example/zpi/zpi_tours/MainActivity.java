467924793a534bfb6ecd90d4946cf1c7e8ada7ff
crecer93 <crecer93@GMAIL.COM>
2015-03-24 23:00:23 +0100

    Login Activity GeneralActivyty (pusty)  Andrzej Jaworski 24.03

--- app/src/main/java/com/example/zpi/zpi_tours/MainActivity.java
+++ app/src/main/java/com/example/zpi/zpi_tours/MainActivity.java
@@ -1,18 +1,61 @@
 package com.example.zpi.zpi_tours;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    Button   buttonLogin;
+    EditText login ;
+    EditText password ;
+    Context context;
+    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonLogin = (Button)findViewById(R.id.buttonLogin);
+        login   = (EditText)findViewById(R.id.textBoxLogin);
+        password   = (EditText)findViewById(R.id.textBoxPass);
+
+        buttonLogin.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        if (login.getText().toString().equals("login")& password.getText().toString().equals("qwerty")){
+                            Log.v("Urzytkownik", login.getText().toString()+" jest zalogowany");
+                            context =getApplicationContext();
+
+                             intent = new Intent(context, GeneralActivity.class );
+                            startActivity(intent);
+                        }else {
+                            Toast toast = Toast.makeText(getApplicationContext(),
+                                    "Hasło lub login jest nie poprawne!", Toast.LENGTH_SHORT);
+                            toast.show();
+                            Log.v("Urzytkownik", login.getText().toString()+" nie jest zalogowany");
+                        }
+
+
+                    }
+                });
+
+
     }
+
+
 
 
     @Override
