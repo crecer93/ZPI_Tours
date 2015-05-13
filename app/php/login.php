2e80150b82a4db5dbbf6442030e6d22658c8190d
Filip-Michalowski <dragim1@wp.pl>
2015-05-13 20:19:18 +0200

    Uaktualnienie plików PHP
    Wszystkie są wrzucone na serwer.

--- app/php/login.php
+++ app/php/login.php
@@ -1,18 +1,7 @@
 <?php
-$host="mysql1.ph-hos.osemka.pl"; //replace with database hostname 
-$username="1430907338_f"; //replace with database username 
-$password="zpi2015"; //replace with database password 
-$db_name="1287631_zpitours"; //replace with database name
+include('baza-danych.php');
 
-//error_reporting(E_ALL ^ E_DEPRECATED);
-$con=mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
-mysql_query("SET NAMES 'utf8' COLLATE 'utf8_polish_ci';");
-mysql_query("SET character_set_client = 'utf8'");
-mysql_query("SET character_set_results = 'utf8'");
-mysql_query("SET character_set_connection = 'utf8'");
-
-$sql = "SELECT email, haslo FROM uzytkownicy"; 
+$sql = "SELECT email, haslo FROM Uzytkownicy"; 
 $result = mysql_query($sql);
 $json = array();
  
--- app/php/uzytkownik.php
+++ app/php/uzytkownik.php
@@ -1,18 +1,7 @@
 <?php
-$host="mysql1.ph-hos.osemka.pl"; //replace with database hostname 
-$username="1430907338_f"; //replace with database username 
-$password="zpi2015"; //replace with database password 
-$db_name="1287631_zpitours"; //replace with database name
+include('baza-danych.php');
 
 $id_uzytkownika = $_POST['id_uzytkownika'];
-
-error_reporting(E_ALL ^ E_DEPRECATED);
-$con=mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
-mysql_query("SET NAMES 'utf8' COLLATE 'utf8_polish_ci';");
-mysql_query("SET character_set_client = 'utf8'");
-mysql_query("SET character_set_results = 'utf8'");
-mysql_query("SET character_set_connection = 'utf8'");
 
 $sql = "SELECT * FROM uzytkownicy WHERE id_uzytkownika=".$id_uzytkownika;
 
